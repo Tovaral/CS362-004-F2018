e4eee80872bff66a12e3b735b68c302a93eb70b3
Alejandro Tovar <tovaral@oregonstate.edu>
2018-10-28 13:53:49 -0700

    Implemented functions

---
+++ projects/Tovaral/quiz/testme.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+	int i = (rand()%(126-32+1) + 32; // this gets a random ASCII value
+	char c = (char) i; // type casts the value into type char
+    return c;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+	char str[7] 	// string length of 6, last will hold null value
+	int i;
+	for(i = 0; i < 6; i+++){
+		int n = (rand()%(126-32+1) + 32; // this gets a random ASCII value
+		char c = (char) n;  // type casts into a char
+		str[i] = c;
+	}
+	str[6] = '\0';
+	char *out = str;
+    return out;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
